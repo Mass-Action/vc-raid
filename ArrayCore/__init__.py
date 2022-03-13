8f5390ac72094ed4be5dc46d361608449cfb09bd
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-13 21:53:23 +0530

    Create __init__.py

---
+++ ArrayCore/__init__.py
@@ -0,0 +1,73 @@
+# ArrayCore - © Noob 
+
+import asyncio
+import os
+import sys
+import time
+
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+    
+__version__ = "v0.0.1"
+
+# -------------CONFIGS--------------------
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+SESSION1 = os.getenv("SESSION", None)
+GROUP_MODE = os.getenv("GROUP_MODE", "True")
+START_VID = os.getenv("START_VID", None)
+
+
+def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
+
+
+sudo = os.getenv("SUDO_USERS")
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
+DEVS = [1517994352, 1789859817, 1432756163]
+for x in DEVS:
+    SUDO_USERS.append(x)
+
+
+# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "1517994352 1789859817 1432756163").split())))
+#----------------------------------------------
+
+vcbot = Client(
+    'ArrayCore',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={'root': 'ArrayCore.vc'},
+)
+
+HELP_DICT = dict()
+hl = HNDLR[0]
+start_time = time.time()
+
+
+if GROUP_MODE == ("True" or "true" or "TRUE"):
+    grp = True
+else:
+    grp = False
+
+
+#-------------------------CLIENTS-----------------------------
+if SESSION1:
+    Venom1 = Client(SESSION1, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'ArrayCore.vc'})
+    call_py1 = PyTgCalls(Venom1)
+else:
+    Venom1 = None
+    call_py1 = None
