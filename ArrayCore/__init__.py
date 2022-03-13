2b043317f8888e535d86f8a65fcfc2e9d873d4b1
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-13 21:54:36 +0530

    Update __init__.py

--- ArrayCore/__init__.py
+++ ArrayCore/__init__.py
@@ -50,7 +50,7 @@ vcbot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins={'root': 'ArrayCore.vc'},
+    plugins={'root': 'ArrayCore.Plugin'},
 )
 
 HELP_DICT = dict()
